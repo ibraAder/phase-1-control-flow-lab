72b9bdab4abf7e7e73ce368b73b2bc6347f44950
ibraAder <127183391+ibraAder@users.noreply.github.com>
2023-03-26 21:41:55 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  if(feet<=400){
+    return"This one is on me!";
+  }
+  else if(feet>2000 && feet<=2500){
+    return'I will gladly take your thirty bucks.';
+  }
+  else if(feet>2500){
+    return'No can do.';
+  }
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+ return (city==='New York') ?'Ok, sounds good.' : "No go.";
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+
+    case 'Generous':
+    return'Thank you so much.';
+    break;
+
+    case'not as generous':
+    return"Thank you.";
+    break;
+
+
+    default:'thanks for everything'
+    return"Bye";
+
+ }
 }
